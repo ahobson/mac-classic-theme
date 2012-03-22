b87f0e2c1eb634f1fae9eb1483efef3d1b8019df
Andrew Hobson <ahobson@damballa.com>
2012-03-22 09:48:16 -0400

    minor updates

--- mac-classic-theme.el
+++ mac-classic-theme.el
@@ -7,7 +7,7 @@
  ;; Font lock faces
  '(font-lock-builtin-face            ((t (:foreground "#0000CD" :bold t))))
  '(font-lock-constant-face           ((t (:foreground "#C5060B" :bold t))))
- '(font-lock-reference-face          ((t (:foreground "#C5060B" :bold t))))
+ '(font-lock-reference-face          ((t (:foreground "#B90690" :bold t))))
  '(font-lock-preprocessor-face       ((t (:foreground "#3596A4"))))
  '(font-lock-keyword-face            ((t (:foreground "#0000FF" :bold t))))
  '(font-lock-type-face               ((t (:foreground "#7175F0"))))
@@ -16,6 +16,7 @@
  '(font-lock-string-face             ((t (:foreground "#036A07"))))
  '(font-lock-comment-face            ((t (:foreground "#0066FF" :italic t))))
  '(font-lock-comment-delimiter-face  ((t (:foreground "#0066FF"))))
+ '(whitespace-trailing               ((t (:background "#FFD0D0"))))
  '(font-lock-doc-face                ((t (:italic t :slant oblique :foreground "#B90690"))))
  '(font-lock-doc-string-face         ((t (:foreground "#B90690")))))
 (provide-theme 'mac-classic)
